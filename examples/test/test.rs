f12a8e3445e53e706817c1f62cfe84af86ab4e8e
alberto.paro <alberto.paro@gmail.com>
2024-01-08 19:20:15 +0100

    Apply cargo fmt

--- examples/test/test.rs
+++ examples/test/test.rs
@@ -5,7 +5,7 @@
 use crate::test_cases::TEST_CASES;
 
 pub fn run_test(configuration: &parse_wiki_text::Configuration) {
-    let mut output = concat!(
+  let mut output = concat!(
         "<title>Parse Wiki Text test cases</title>",
         "<style>",
         "a{color:#006064;display:block;padding:8;text-decoration:none}",
@@ -19,52 +19,47 @@ pub fn run_test(configuration: &parse_wiki_text::Configuration) {
         "</style>",
         "<div style=\"background:#fff;box-shadow: 0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);flex:0 1 220px;overflow:auto\">"
     ).to_owned();
-    if let Some(window) = TEST_CASES
-        .windows(2)
-        .find(|window| window[0].0 >= window[1].0)
-    {
-        panic!("Sort: {:#?}", (window[0].0, window[1].0));
+  if let Some(window) = TEST_CASES.windows(2).find(|window| window[0].0 >= window[1].0) {
+    panic!("Sort: {:#?}", (window[0].0, window[1].0));
+  }
+  for (title, test_cases) in TEST_CASES {
+    if let Some(window) = test_cases.windows(2).find(|window| window[0] >= window[1]) {
+      panic!("Sort: {:#?}", window);
     }
-    for (title, test_cases) in TEST_CASES {
-        if let Some(window) = test_cases.windows(2).find(|window| window[0] >= window[1]) {
-            panic!("Sort: {:#?}", window);
+    output += &format!("<a href=#{}>", title.replace(" ", "_"));
+    output += title;
+    output += &format!(" <span>{}</span></a>", test_cases.len());
+  }
+  output += "</div><div style=\"flex:1 1 200px;overflow:auto\">";
+  for (title, test_cases) in TEST_CASES {
+    output += &format!("<h1 id={}>", title.replace(" ", "_"));
+    output += title;
+    output += "</h1>";
+    for wiki_text in *test_cases {
+      output += "<div><pre>";
+      output += &wiki_text
+        .replace("&", "&amp;")
+        .replace("<", "&lt;")
+        .replace("\t", "<span>⭾</span>")
+        .replace("\n", "<span>⏎</span>\n")
+        .replace(" ", "<span>·</span>")
+        .replace("</span><span>", "");
+      match std::panic::catch_unwind(|| configuration.parse(wiki_text)) {
+        Err(_) => {
+          eprintln!("Panic with wiki text {:?}", wiki_text);
+          output += "</pre><hr>panic</div>";
         }
-        output += &format!("<a href=#{}>", title.replace(" ", "_"));
-        output += title;
-        output += &format!(" <span>{}</span></a>", test_cases.len());
-    }
-    output += "</div><div style=\"flex:1 1 200px;overflow:auto\">";
-    for (title, test_cases) in TEST_CASES {
-        output += &format!("<h1 id={}>", title.replace(" ", "_"));
-        output += title;
-        output += "</h1>";
-        for wiki_text in *test_cases {
-            output += "<div><pre>";
-            output += &wiki_text
-                .replace("&", "&amp;")
-                .replace("<", "&lt;")
-                .replace("\t", "<span>⭾</span>")
-                .replace("\n", "<span>⏎</span>\n")
-                .replace(" ", "<span>·</span>")
-                .replace("</span><span>", "");
-            match std::panic::catch_unwind(|| configuration.parse(wiki_text)) {
-                Err(_) => {
-                    eprintln!("Panic with wiki text {:?}", wiki_text);
-                    output += "</pre><hr>panic</div>";
-                }
-                Ok(result) => {
-                    output += "</pre><hr><pre>";
-                    output += &format!("{:#?}", result)
-                        .replace("&", "&amp;")
-                        .replace("<", "&lt;");
-                    output += "</pre></div>";
-                }
-            }
+        Ok(result) => {
+          output += "</pre><hr><pre>";
+          output += &format!("{:#?}", result).replace("&", "&amp;").replace("<", "&lt;");
+          output += "</pre></div>";
         }
+      }
     }
-    output += "</div>";
-    if let Err(error) = std::fs::write("report.html", output) {
-        eprintln!("Failed to write report: {}", error);
-        std::process::exit(1);
-    }
+  }
+  output += "</div>";
+  if let Err(error) = std::fs::write("report.html", output) {
+    eprintln!("Failed to write report: {}", error);
+    std::process::exit(1);
+  }
 }
--- tests/main.rs
+++ tests/main.rs
@@ -1,7 +1,7 @@
 use parse_wiki_text::Configuration;
 #[test]
 fn test() {
-    let result = Configuration::default().parse(TRICKY_WIKITEXT);
+  let result = Configuration::default().parse(TRICKY_WIKITEXT);
 }
 
 // this resulted in a seemingly infinite loop, from the Kid Chocolate wiki page
